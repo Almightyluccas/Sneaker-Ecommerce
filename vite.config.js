03bf6cce65428ab4c83bce7428e46c79268336db
Luccas Amorim <117477920+Almightyluccas@users.noreply.github.com>
2025-07-03 22:33:41 -0300

    Fixed schema issue.

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,16 @@ import tailwindcss from '@tailwindcss/vite';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: [
+                'resources/css/app.css',
+                'resources/js/app.js',
+                'resources/js/theme-switcher.js',
+                'resources/js/shop.js',
+                'resources/js/product-page.js',
+                'resources/js/cart.js',
+                'resources/js/navbar.js',
+            ],
+
             refresh: true,
         }),
         tailwindcss(),
